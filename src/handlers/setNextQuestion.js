652a9f694749b0bde56d13f5196969b287d1665f
Hazem Bittar <mr.hazem@hotmail.fr>
2020-08-20 22:56:59 +0200

    Delete setNextQuestion.js

--- src/handlers/setNextQuestion.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-console.log('--- loading handler: setNextQuestion');
-
-
-function setNextQuestion() {
-    resetState()
-    showQuestion(shuffledQuestions[currentQuestionIndex])
-  }
